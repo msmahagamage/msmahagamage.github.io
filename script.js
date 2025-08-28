2ba5b1b9407205c028c6e1f90b5a05015ed289c6
madushamahagamage <msmahagam@gmail.com>
2025-08-28 02:29:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// This is a simple script to show that your JS file is connected.
+console.log("Welcome to my website!");
+
+// You can add more interactive features here later,
+// like a contact form validator or interactive elements.
